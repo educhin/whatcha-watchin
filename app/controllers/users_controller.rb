75a830579a53710e810947ccbfe26e5c89ac7262
Chinedu Arinze <c.s.arinze@gmail.com>
2019-10-17 02:10:40 +0000

    add post route for user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,21 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
     else
+      # fix this below
       redirect to '/some_link'
     end
   end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      # fix this below
+      redirect to '/some_link'
+    end
+  end
+
 end
