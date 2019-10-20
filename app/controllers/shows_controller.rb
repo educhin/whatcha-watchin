213f1a357d1537b2545c82ae60bdbb59c4696304
Chinedu Arinze <c.s.arinze@gmail.com>
2019-10-20 19:33:48 +0000

    add minor functionality and route to Shows controller.

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,3 +1,10 @@
 class ShowsController < ApplicationController
-    
+    get '/myshows' do
+      if logged_in? && current_user
+        erb :'shows/user_shows', locals: {message: "Please login before viewing this page"}
+      else
+        # fix this below
+        redirect to '/some_link'
+      end
+    end
 end
