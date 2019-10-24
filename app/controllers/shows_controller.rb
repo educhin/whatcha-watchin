787df4cdbb6ec3aac877cf19213ac81dcae4a621
Chinedu Arinze <c.s.arinze@gmail.com>
2019-10-24 20:03:24 +0000

    add sessions, new route for shows (get/post)

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -4,7 +4,43 @@ class ShowsController < ApplicationController
         erb :'shows/user_shows', locals: {message: "Please login before viewing this page"}
       else
         # fix this below
-        redirect to '/some_link'
+        redirect to '/login'
+
       end
+    end
+
+    get '/shows/:user_slug' do
+
+    end
+
+    get '/shows/new' do
+      if logged_in?
+        erb :'shows/add_show'
+      else
+        redirect to '/login'
+      end
+    end
+
+    post '/shows' do
+      if params[:name] != "" && params[:genre] != ""
+        @user = User.find(session[:user_id])
+        @show = Show.new(params[:show])
+        @show.user_id = @user.id
+        @show.save
+      else
+        redirect to '/shows/new'
+      end
+    end
+
+    get 'shows/:id/edit' do
+
+    end
+
+    patch '/shows/:id' do
+
+    end
+
+    delete '/shows/:id/delete' do
+
     end
 end
